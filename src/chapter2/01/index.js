81131a18de38b0e3f4a7533cce6068910544cf18
ykwan0714 <ykwan0714@gmail.com>
2022-07-20 00:23:58 +0900

    Add 챕터 2. 초보자를 위한 css 애니메이션 기초 학습하기 - 예제 01

--- src/chapter2/01/index.js
+++ src/chapter2/01/index.js
@@ -1,8 +1,98 @@
 import '/assets/css/common.css'
 import './index.css'
+import { $ } from '/assets/js/common.js'
+import jakeImg from '/assets/images/jake.png'
 ;(() => {
   const app = document.getElementById('App')
-  app.innerHTML = ``
+  app.innerHTML = `<div class="wrap">
+  <section class="sec01">
+    <h2 class="tit">TRANSFORM</h2>
+    <article class="transfom_ex">
+      <ul class="list">
+        <li>
+          <figure class="jake jake01">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">ROTATE</strong>
+        </li>
+        <li>
+          <figure class="jake jake02">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">SCALE</strong>
+        </li>
+        <li>
+          <figure class="jake jake03">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">SKEW</strong>
+        </li>
+        <li>
+          <figure class="jake jake04">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">TRANSLATE</strong>
+        </li>
+      </ul>
+      <button class="btn start">START</button>
+      <button class="btn reset">RESET</button>
+    </article>
+  </section>
+  <section class="sec02">
+    <h2 class="tit">TRANSFORM & TRANSITION</h2>
+    <article class="transfom_ex">
+      <ul class="list">
+        <li>
+          <figure class="jake jake01">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">ROTATE</strong>
+        </li>
+        <li>
+          <figure class="jake jake02">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">SCALE</strong>
+        </li>
+        <li>
+          <figure class="jake jake03">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">SKEW</strong>
+        </li>
+        <li>
+          <figure class="jake jake04">
+            <img src="${jakeImg}" />
+          </figure>
+          <strong class="txt">TRANSLATE</strong>
+        </li>
+      </ul>
+      <button class="btn start">START</button>
+      <button class="btn reset">RESET</button>
+    </article>
+  </section>
+</div>`
 
-  window.addEventListener('load', () => {})
+  window.addEventListener('load', () => {
+    const time = 500
+    $('.sec01 .btn.start').addEventListener('click', () => {
+      $('.sec01 .jake').forEach((el, idx) => {
+        setTimeout(() => el.classList.add('active'), time * idx)
+      })
+    })
+    $('.sec01 .btn.reset').addEventListener('click', () => {
+      $('.sec01 .jake').forEach(el=> el.classList.remove('active'))
+    })
+
+    $('.sec02 .btn.start').addEventListener('click', () => {
+      $('.sec02 .jake').forEach((el, idx) => {
+        setTimeout(() => el.classList.add('active'), time * idx)
+      })
+    })
+    $('.sec02 .btn.reset').addEventListener('click', () => {
+      $('.sec02 .jake').forEach(el=> el.classList.remove('active'))
+    })
+
+
+  })
 })()
---
+++ src/chapter2/README.md
@@ -0,0 +1,11 @@
+# 챕터 2. 초보자를 위한 css 애니메이션 기초 학습하기
+
+## 예제 01
+CSS transform의 여러가지 function을 활용하여 이미지를 조작한다.
+transition 유무에 따른 차이를 이해한다.
+- [CSS transform](https://developer.mozilla.org/en-US/docs/Web/CSS/transform)
+  - [CSS transform-function](https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function)
+- [CSS transition](https://developer.mozilla.org/en-US/docs/Web/CSS/transition)
+## 예제 02
+
+## 예제 03
